9fcefb0223123830e1305ab08dddda671456da20
TANISHQ TIWARI <144956446+txnishq28@users.noreply.github.com>
2025-07-17 16:00:29 +0530

    Update Article.ts

--- src/types/Article.ts
+++ src/types/Article.ts
@@ -1,13 +1,10 @@
 export interface Article {
-  source?: {
-    id?: string | null
-    name?: string
-  }
-  author?: string | null
+  source: { id: string | null; name: string }
+  author: string | null
   title: string
-  description?: string
+  description: string
   url: string
-  urlToImage?: string
-  publishedAt?: string
-  content?: string
+  urlToImage: string
+  publishedAt: string
+  content: string
 }
