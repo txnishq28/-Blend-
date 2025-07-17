30cbee140d349902aa0d9fda633c15c0ebd1a738
TANISHQ TIWARI <144956446+txnishq28@users.noreply.github.com>
2025-07-17 16:16:57 +0530

    Update SettingsPanel.tsx

--- src/components/SettingsPanel.tsx
+++ src/components/SettingsPanel.tsx
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { RootState } from '@/store'
+import { RootState } from '@/store/store'
 import { setCategories } from '@/store/preferencesSlice'
 
 export default function SettingsPanel() {
