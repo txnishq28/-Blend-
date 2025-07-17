9bd5a631467865ab0c24ee6d089535b60543bc56
TANISHQ TIWARI <144956446+txnishq28@users.noreply.github.com>
2025-07-17 16:20:59 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,9 @@
-// src/pages/_app.tsx
-
 import { Provider } from 'react-redux'
 import { store } from '@/store/store'
 import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
