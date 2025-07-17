0f3db4fc96fae0df141d0c0585dd6b5367652692
TANISHQ TIWARI <144956446+txnishq28@users.noreply.github.com>
2025-07-17 16:43:05 +0530

    Update newsApiSlice.ts

--- src/store/newsApiSlice.ts
+++ src/store/newsApiSlice.ts
@@ -1,19 +1,15 @@
-// src/store/newsApiSlice.ts
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-
-const NEWS_API_KEY = process.env.NEXT_PUBLIC_NEWS_API_KEY as string
-console.log('ENV API KEY:', NEWS_API_KEY)
 
 export const newsApi = createApi({
   reducerPath: 'newsApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://newsapi.org/v2/',
+    baseUrl: '/api/', // 👈 calls your own server route!
   }),
   endpoints: (builder) => ({
     getTopHeadlines: builder.query({
       query: (categories: string[]) => {
         const categoryQuery = categories.length > 0 ? categories[0] : 'general'
-        return `top-headlines?country=us&category=${categoryQuery}&apiKey=${NEWS_API_KEY}`
+        return `news?category=${categoryQuery}`
       },
     }),
   }),
