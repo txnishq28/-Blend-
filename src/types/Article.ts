0c0c7327916078c008bffba6035918b0e9d29fac
TANISHQ TIWARI <144956446+txnishq28@users.noreply.github.com>
2025-07-17 15:52:09 +0530

    Update Article.ts

--- src/types/Article.ts
+++ src/types/Article.ts
@@ -1,7 +1,9 @@
-// src/types/Article.ts
-export interface Article {
-  url: string
-  urlToImage?: string
-  title: string
-  description?: string
-}
+import { Article } from '@/types/Article'
+
+const favoriteArticles = data.articles.filter((article: Article) =>
+  favorites.includes(article.url)
+)
+
+...
+
+{favoriteArticles.map((article: Article) => (...))}
